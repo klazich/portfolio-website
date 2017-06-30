482ea110710a10e44d1e7c05cf6d9d002152b1ca
Kevin <kevin.lazich@gmail.com>
2017-06-30 02:38:55 -0400

    first commit to new repo

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+let gulp = require('gulp')
+let postcss = require('gulp-postcss')
+let rename = require('gulp-rename')
+let sourcemap = require('gulp-sourcemaps')
+let del = require('del')
+
+gulp.task('clean', function () {
+    return del(['www/css/**/*'])
+})
+
+gulp.task('css', ['clean'], function () {
+    return gulp.src('css/*.css')
+        .pipe(postcss([
+            require('postcss-import'),
+            require('postcss-font-magician'),
+            require('postcss-custom-media'),
+            require('postcss-custom-properties'),
+            require('postcss-calc'),
+            require('postcss-color-function'),
+            //require('postcss-discard-comments'),
+            require('autoprefixer'),
+        ]))
+        .pipe(gulp.dest('www/css'))
+})
+
+gulp.task('minify', ['css'], function () {
+    return gulp.src(['www/css/*.css'])
+        .pipe(sourcemap.init())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(postcss([require('cssnano')]))
+        .pipe(sourcemap.write('.'))
+        .pipe(gulp.dest('www/css'))
+})
+
+gulp.task('default', ['minify'])
