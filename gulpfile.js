780ee47e921f59eddccffce6f70bd8d0c6268d31
Kevin <kevin.lazich@gmail.com>
2017-07-05 01:32:57 -0400

    Adds additional Gulp plugins, removes some PostCSS plugins
    - gulp-imagemin to compress images.
    - gulp-stylefmt to format readable css.
    - gulp-html W3C html validation API

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,41 @@ let gulp = require('gulp')
 let postcss = require('gulp-postcss')
 let rename = require('gulp-rename')
 let sourcemap = require('gulp-sourcemaps')
+let validator = require('gulp-html')
+let imagemin = require('gulp-imagemin')
+let stylefmt = require('gulp-stylefmt')
 let del = require('del')
 
+let paths = {
+    css: 'src/css/**/*.css',
+    html: 'src/index.html'
+}
+
 gulp.task('clean', function () {
-    return del(['docs/css/**/*', 'docs/index.html'])
+    return del(['docs'])
 })
 
-gulp.task('html', function () {
+gulp.task('images', ['clean'], function () {
+    return gulp.src('src/img/**/*')
+        .pipe(imagemin([
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({ plugins: [{ removeViewBox: true }] })
+        ], {
+            verbose: true
+        }))
+        .pipe(gulp.dest('docs/img'))
+})
+
+gulp.task('html', ['clean'], function () {
     return gulp.src('src/index.html')
+        .pipe(validator({ verbose: true }))  // w3c html validation
         .pipe(gulp.dest('docs/'))
 })
 
 gulp.task('css', ['clean'], function () {
     return gulp.src('src/css/base.css')
         .pipe(postcss([
-            //require('postcss-processor-order'),
             require('postcss-import'),
             require('postcss-url'),
             require('postcss-cssnext'),
@@ -26,10 +46,9 @@ gulp.task('css', ['clean'], function () {
             require('postcss-custom-properties'),
             require('postcss-calc'),
             require('postcss-color-function'),
-            require('postcss-browser-reporter'),
             require('postcss-reporter')
-
         ]))
+        .pipe(stylefmt())
         .pipe(gulp.dest('docs/css'))
 })
 
@@ -37,9 +56,12 @@ gulp.task('minify', ['css'], function () {
     return gulp.src(['docs/css/*.css'])
         .pipe(sourcemap.init())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(postcss([require('cssnano')({autoprefixer: false})]))
+        .pipe(postcss([require('cssnano')({ autoprefixer: false })]))
         .pipe(sourcemap.write('.'))
         .pipe(gulp.dest('docs/css'))
 })
 
-gulp.task('default', ['minify', 'html'])
+gulp.task('default', ['minify', 'html', 'images'], function () {
+    return gulp.src('src/CNAME')
+        .pipe(gulp.dest('docs/'))
+})
