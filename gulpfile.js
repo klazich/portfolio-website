84c2535e1005013fbf1c6b4de278b8e941ebe3e9
Kevin <kevin.lazich@gmail.com>
2017-07-16 00:22:55 -0400

    Only img/min images will build, add img/org to .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,72 @@
-let gulp = require('gulp')
-let postcss = require('gulp-postcss')
-let rename = require('gulp-rename')
-let sourcemap = require('gulp-sourcemaps')
-let validator = require('gulp-html')
-let stylefmt = require('gulp-stylefmt')
-let del = require('del')
+let gulp = require('gulp');
+let postcss = require('gulp-postcss');
+let rename = require('gulp-rename');
+let sourcemap = require('gulp-sourcemaps');
+let validator = require('gulp-html');
+let stylefmt = require('gulp-stylefmt');
+let del = require('del');
 
 let op = {
   uncss: {
-    html: ['www/index.html']
+    html: ['www/index.html'],
   },
   cssnano: {
-    autoprefixer: false
-  }
-}
+    autoprefixer: false,
+  },
+};
 
 /**
  * Clean tasks
  */
 gulp.task('clean:img', function() {
-  return del(['www/img/**/*'])
-})
+  return del(['www/img/**/*']);
+});
 
 gulp.task('clean:html', function() {
-  return del(['www/*.html'])
-})
+  return del(['www/*.html']);
+});
 
 gulp.task('clean:css', function() {
-  return del(['www/css/*.css', 'www/css/*.map'])
-})
+  return del(['www/css/*.css', 'www/css/*.map']);
+});
 
 gulp.task('clean', function() {
-  return del(['www/**'])
-})
+  return del(['www/**']);
+});
 
 /**
  * Font tasks
  */
-gulp.task('fonts', ['clean'], function () {
-  return gulp.src('src/font/**/*').pipe(gulp.dest('www/font'))
-})
+gulp.task('fonts', ['clean'], function() {
+  return gulp.src('src/font/**/*').pipe(gulp.dest('www/font'));
+});
 
 /**
  * Image tasks
  */
 gulp.task('images', ['clean'], function() {
-  return gulp.src('src/img/**/*').pipe(gulp.dest('www/img'))
-})
+  return gulp.src([
+    'src/img/**/*',
+    '!src/img/org/**']).pipe(gulp.dest('www/img'));
+});
 
 /**
  * Javascript tasks
  */
-gulp.task('js', ['clean'], function () {
-  return gulp.src('src/js/*.js').pipe(gulp.dest('www/js'))
-})
+gulp.task('js', ['clean'], function() {
+  return gulp.src('src/js/*.js').pipe(gulp.dest('www/js'));
+});
 
 /**
  * HTML tasks
  */
 gulp.task('html', ['clean'], function() {
-  return gulp.src('src/index.html').pipe(gulp.dest('www'))
-})
+  return gulp.src('src/index.html').pipe(gulp.dest('www'));
+});
 
 gulp.task('html:validate', ['html'], function() {
-  return gulp.src('www/index.html').pipe(validator({ verbose: true }))
-})
+  return gulp.src('www/index.html').pipe(validator({verbose: true}));
+});
 
 /**
  * CSS tasks
@@ -74,42 +76,40 @@ gulp.task('css', ['clean', 'html'], function() {
     require('postcss-import'),
     require('postcss-url'),
     require('postcss-discard-comments'),
-    require('postcss-uncss')(op.uncss),
+    //require('postcss-uncss')(op.uncss),
     require('postcss-custom-properties'),
     require('postcss-custom-media'),
     require('css-mqpacker'),
     require('postcss-cssnext'),
-    require('postcss-reporter')
-  ]
+    require('postcss-reporter'),
+  ];
 
-  return gulp
-    .src('src/css/styles.css')
-    .pipe(postcss(processors))
-    .pipe(stylefmt())
-    .pipe(gulp.dest('www/css'))
-})
+  return gulp.src('src/css/styles.css').
+    pipe(postcss(processors)).
+    pipe(stylefmt()).
+    pipe(gulp.dest('www/css'));
+});
 
 /**
  * CSS minification tasks
  */
 gulp.task('minify', ['clean', 'css'], function() {
-  return gulp
-    .src('www/css/styles.css')
-    .pipe(sourcemap.init())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(postcss([require('cssnano')(op.cssnano)]))
-    .pipe(sourcemap.write('.'))
-    .pipe(gulp.dest('www/css'))
-})
+  return gulp.src('www/css/styles.css').
+    pipe(sourcemap.init()).
+    pipe(rename({suffix: '.min'})).
+    pipe(postcss([require('cssnano')(op.cssnano)])).
+    pipe(sourcemap.write('.')).
+    pipe(gulp.dest('www/css'));
+});
 
 /**
  * Watch task
  */
-gulp.task('watch', function() {})
+gulp.task('watch', function() {});
 
 /**
  * Default task
  */
 gulp.task('default', ['clean', 'minify', 'js', 'images', 'fonts'], function() {
-  return gulp.src(['src/CNAME', 'src/manifest.json']).pipe(gulp.dest('www/'))
-})
+  return gulp.src(['src/CNAME', 'src/manifest.json']).pipe(gulp.dest('www/'));
+});
