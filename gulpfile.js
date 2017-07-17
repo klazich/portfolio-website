a86f2d3d2409f3db75402fa492238d0b45c6ecb7
Kevin <kevin.lazich@gmail.com>
2017-07-17 19:57:51 -0400

    Remove babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,78 @@
-let gulp = require('gulp');
-let postcss = require('gulp-postcss');
-let rename = require('gulp-rename');
-let sourcemap = require('gulp-sourcemaps');
-let validator = require('gulp-html');
-let stylefmt = require('gulp-stylefmt');
-let del = require('del');
+let gulp = require('gulp')
+let postcss = require('gulp-postcss')
+let rename = require('gulp-rename')
+let sourcemap = require('gulp-sourcemaps')
+let validator = require('gulp-html')
+let stylefmt = require('gulp-stylefmt')
+let del = require('del')
 
 let op = {
-  uncss: {
-    html: ['www/index.html'],
+  uncss  : {
+    html: [ 'www/index.html' ],
   },
   cssnano: {
     autoprefixer: false,
   },
-};
+}
 
 /**
  * Clean tasks
  */
 gulp.task('clean:img', function() {
-  return del(['www/img/**/*']);
-});
+  return del([ 'www/img/**/*' ])
+})
 
 gulp.task('clean:html', function() {
-  return del(['www/*.html']);
-});
+  return del([ 'www/*.html' ])
+})
 
 gulp.task('clean:css', function() {
-  return del(['www/css/*.css', 'www/css/*.map']);
-});
+  return del([ 'www/css/*.css', 'www/css/*.map' ])
+})
 
 gulp.task('clean', function() {
-  return del(['www/**']);
-});
+  return del([ 'www/**' ])
+})
 
 /**
  * Font tasks
  */
-gulp.task('fonts', ['clean'], function() {
-  return gulp.src('src/font/**/*').pipe(gulp.dest('www/font'));
-});
+gulp.task('fonts', [ 'clean' ], function() {
+  return gulp.src('src/font/**/*').pipe(gulp.dest('www/font'))
+})
 
 /**
  * Image tasks
  */
-gulp.task('images', ['clean'], function() {
+gulp.task('images', [ 'clean' ], function() {
   return gulp.src([
     'src/img/**/*',
-    '!src/img/org/**']).pipe(gulp.dest('www/img'));
-});
+    '!src/img/org/**' ]).pipe(gulp.dest('www/img'))
+})
 
 /**
  * Javascript tasks
  */
-gulp.task('js', ['clean'], function() {
-  return gulp.src('src/js/*.js').pipe(gulp.dest('www/js'));
-});
+gulp.task('js', [ 'clean' ], function() {
+  return gulp.src('src/js/*.js')
+    .pipe(gulp.dest('www/js'))
+})
 
 /**
  * HTML tasks
  */
-gulp.task('html', ['clean'], function() {
-  return gulp.src('src/index.html').pipe(gulp.dest('www'));
-});
+gulp.task('html', [ 'clean' ], function() {
+  return gulp.src('src/index.html').pipe(gulp.dest('www'))
+})
 
-gulp.task('html:validate', ['html'], function() {
-  return gulp.src('www/index.html').pipe(validator({verbose: true}));
-});
+gulp.task('html:validate', [ 'html' ], function() {
+  return gulp.src('www/index.html').pipe(validator({ verbose: true }))
+})
 
 /**
  * CSS tasks
  */
-gulp.task('css', ['clean', 'html'], function() {
+gulp.task('css', [ 'clean', 'html' ], function() {
   let processors = [
     require('postcss-import'),
     require('postcss-url'),
@@ -82,34 +83,36 @@ gulp.task('css', ['clean', 'html'], function() {
     require('css-mqpacker'),
     require('postcss-cssnext'),
     require('postcss-reporter'),
-  ];
+  ]
 
-  return gulp.src('src/css/styles.css').
-    pipe(postcss(processors)).
-    pipe(stylefmt()).
-    pipe(gulp.dest('www/css'));
-});
+  return gulp.src('src/css/styles.css')
+    .pipe(postcss(processors))
+    .pipe(stylefmt())
+    .pipe(gulp.dest('www/css'))
+})
 
 /**
  * CSS minification tasks
  */
-gulp.task('minify', ['clean', 'css'], function() {
-  return gulp.src('www/css/styles.css').
-    pipe(sourcemap.init()).
-    pipe(rename({suffix: '.min'})).
-    pipe(postcss([require('cssnano')(op.cssnano)])).
-    pipe(sourcemap.write('.')).
-    pipe(gulp.dest('www/css'));
-});
+gulp.task('minify', [ 'clean', 'css' ], function() {
+  return gulp.src('www/css/styles.css')
+    .pipe(sourcemap.init())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(postcss([ require('cssnano')(op.cssnano) ]))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('www/css'))
+})
 
 /**
  * Watch task
  */
-gulp.task('watch', function() {});
+gulp.task('watch', function() {})
 
 /**
  * Default task
  */
-gulp.task('default', ['clean', 'minify', 'js', 'images', 'fonts'], function() {
-  return gulp.src(['src/CNAME', 'src/manifest.json']).pipe(gulp.dest('www/'));
-});
+gulp.task('default', [ 'clean', 'minify', 'js', 'images', 'fonts' ],
+  function() {
+    return gulp.src([ 'src/CNAME', 'src/manifest.json' ])
+      .pipe(gulp.dest('www/'))
+  })
